452a6570ee3d9b8e33db2610bd027c54239bcd55
Michael Nelson <42mnelson@gmail.com>
2014-02-17 10:42:38 +0000

    Fix creation of RelocaTE specific reference TE annotation file

--- scripts/runrelocate.sh
+++ scripts/runrelocate.sh
@@ -3,7 +3,7 @@
 if (( $# > 0 ))
 then
 	# Run the relocaTE pipeline
-	awk -F'[\t=]' '{print $9"\t"$1":"$4".."$5}' $5 > annotation
+	awk -F'[\t]' '{print $3"\t"$1":"$4".."$5}' $5 > annotation
 	perl scripts/relocaTE.pl -t $1 -d $3 -g $2 -1 _1 -2 _2 -o $4 -r annotation
 	
 	# Extract the relevant information from the output files for each TE and collate them.
