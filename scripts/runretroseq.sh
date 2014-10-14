5263497f156e242a0518a3daa859785b9ca15ee4
Michael Nelson <42mnelson@gmail.com>
2014-10-14 15:31:56 +0100

    Filter for TE insertions near to reference TEs

--- scripts/runretroseq.sh
+++ scripts/runretroseq.sh
@@ -35,12 +35,12 @@ then
 	# bin/retroseq.pl -discover -bam $2 -eref $reference"_elementlist" -output $samplename/$samplename.discovery
 	bin/retroseq.pl -discover -bam $2 -refTEs $reference"_locationlist" -output $samplename/$samplename.discovery
 
-	bin/retroseq.pl -call -bam $2 -input $samplename/$samplename.discovery -ref $3 -output $samplename/$samplename.calling -orientate yes
+	bin/retroseq.pl -call -bam $2 -input $samplename/$samplename.discovery -filter $reference"_locationlist" -ref $3 -output $samplename/$samplename.calling -orientate yes
 
 	# Extract the relevant results
 	echo -e "track name=\"$samplename"_RetroSeq"\" description=\"$samplename"_RetroSeq"\"" > $samplename/$samplename"_retroseq.bed"
 	awk '$1!~/#/{print $0}' $samplename/$samplename.calling.PE.vcf >> $samplename/tmp
-	awk -F'[=,\t:]' '{if ($20 >= 6) print $1"\t"$11"\t"$12"\t"$10"_new\t0\t."}' $samplename/tmp >> $samplename/$samplename"_retroseq_presort.bed"
+	awk -F'[=,\t:]' '{if ($21 >= 6) print $1"\t"$11"\t"$12"\t"$10"_new\t0\t."}' $samplename/tmp >> $samplename/$samplename"_retroseq_presort.bed"
 	bedtools sort -i $samplename/$samplename"_retroseq_presort.bed" >> $samplename/$samplename"_retroseq.bed"
 	rm $samplename/tmp $samplename/$samplename"_retroseq_presort.bed"
 
