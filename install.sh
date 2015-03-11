f3bb62f7818b2d125109a27b9fef35fecf1a34b3
Michael Nelson <42mnelson@gmail.com>
2015-03-11 12:28:32 +0000

    Small fix to last update

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ cp scripts/runretroseq.sh RetroSeq
 cp scripts/splitforRetroSeq.pl RetroSeq
 cp scripts/runrelocate.sh RelocaTE
 cp scripts/runtelocate.sh TE-locate
-mv scripts/genomes1001_data_processing/Functions.class TE-locate
+mv scripts/genomes1001_data_processing TE-locate
 cd TE-locate
 jar uf TE_locate_core.jar genomes1001_data_processing/Functions.class
 cd ..
