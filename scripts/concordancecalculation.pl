9f53a5b4e1fc977b22a77babfb24461face407d3
Michael Nelson <42mnelson@gmail.com>
2015-08-05 17:38:52 +0100

    Edited PoPoolationTE parsing

--- scripts/concordancecalculation.pl
+++ scripts/concordancecalculation.pl
@@ -7,36 +7,32 @@ use File::Basename;
 # The output is a tab delimited file with the following columns:
 # SampleID	Method1name	Method2name	Concordantnovelpredictions	Totalnovelpredictionshigh	Totalnovelpredictionslow	Concordantexistingpredictions	Totalexistingpredictionshigh	Totalexistingpredictionslow
 
+my $window = @ARGV[0];
 # For each directory supplied (these should be the directories in the results folder to be analysed)
-foreach my $directory (@ARGV)
+foreach my $directory (@ARGV[1..$#ARGV])
 {
+	if ($directory =~ /reference/)
+	{
+		next;
+	}
 	# Open the relevant folder, this will be the results for one sample
 	opendir my $sample, "$directory/results";
 	
 	$sampleID = basename($directory);
 	
-	# Read the results files (one for each different method), ignoring dotfiles
-	my @files = grep {!/^\./} readdir $sample;
-	for (my $i = 0; $i < @files; $i++)
-	{	
-		# Strip the decimal from any coordinates in the PoPoolationTE results file that have a non integer value	
-		if (@files[$i] =~ /popoolationte/)
-		{
-			$old = @files[$i];
-			open (TMP, ">$directory/results/@files[$i].nodec");
-			print TMP "track name=\"$sampleID\_PoPoolationTE_nodec\" description=\"$sampleID\_PoPoolationTE_nodec\"\n";
-			system ("awk '\$2=int(\$2),\$3=int(\$3)' OFS=\"\\t\" \"$directory/results/@files[$i]\" >> \"$directory/results/@files[$i].nodec\"");
-			@files[$i] = "$old.nodec";
-			close (TMP);
-		}
-	}
-	
-	open (NEWOUTFILE, ">$directory/results/$sampleID.new.concordance.tsv");	
-	open (OLDOUTFILE, ">$directory/results/$sampleID.old.concordance.tsv");	
+	# Read the results files (one for each different method)
+	my @files = grep {/non/} readdir $sample;
+	open (NEWOUTFILE, ">$directory/results/$sampleID.$window.new.concordance.tsv");	
+	open (OLDOUTFILE, ">$directory/results/$sampleID.$window.old.concordance.tsv");	
 	$count = 0;
 	
 	# Sort the files list to create more standardised output
-	@files = sort @files;	
+	@files = sort @files;
+	
+	# Use indexes to sort into order used for rest of thesis.
+	my @sortorder = qw/ 1 5 2 4 6 3/;	
+	my @idx = sort { $sortorder[$a] <=> $sortorder[$b] } 0 .. $#sortorder;
+	@files = @files[@idx];
 	
 	# For each results file
 	foreach my $file1 (@files)
@@ -50,9 +46,9 @@ foreach my $directory (@ARGV)
 			
 			# Print the names of the methods being compared
 			@method1 = split(/_/, $file1, 2);
-			@method1name = split(/\./, @method1[1], 2);
+			@method1name = split(/_non/, @method1[1], 2);
 			@method2 = split(/_/, @files[$c], 2);
-			@method2name = split(/\./, @method2[1], 2);
+			@method2name = split(/_non/, @method2[1], 2);
 			
 			print NEWOUTFILE "@method1name[0] \t @method2name[0]\t";
 			if (@method1name[0] ne "retroseq" && @method2name[0] ne "retroseq")
@@ -61,21 +57,24 @@ foreach my $directory (@ARGV)
 			}
 			
 			# Run bedtools window to detect overlapping predictions
-			@result = `bedtools window -w 100 -a $directory/results/$file1 -b $file2 2>&1`;
+			@result = `bedtools window -w $window -a $directory/results/$file1 -b $file2 2>&1`;
+			
 			$newcount = 0;
 			$oldcount = 0;
 			foreach my $line (@result)
 			{
 				my @fields = split "\t" , $line;
+				my @te1 = split(/_r|_n/, @fields[3], 2);
+				my @te2 = split(/_r|_n/, @fields[9], 2);
 				# Only count predictions of the same TE as concordant
-				if (@fields[3] eq @fields[9])
+				if (@te1[0] eq @te2[0])
 				{
 					# Keep separate counts for new and old TE sites
-					if (@fields[3] =~ /new/)
+					if (@fields[3] =~ /non-/ && @fields[9] =~ /non-/)
 					{
 						$newcount++;
 					}
-					if (@fields[3] =~ /old/)
+					if (@fields[3] =~ /_reference/ && @fields[9] =~ /_reference/)
 					{
 						$oldcount++;
 					}
@@ -86,8 +85,8 @@ foreach my $directory (@ARGV)
 			print NEWOUTFILE "$newcount \t";
 			
 			# Print the total number of novel TE predictions for each method, high value first then low
-			my $newpredictionsmethod1=`grep new "$directory/results/$file1" | wc -l`;
-			my $newpredictionsmethod2=`grep new $file2 | wc -l`;
+			my $newpredictionsmethod1=`grep non- "$directory/results/$file1" | wc -l`;
+			my $newpredictionsmethod2=`grep non- $file2 | wc -l`;
 			
 			chomp ($newpredictionsmethod1);
 			chomp ($newpredictionsmethod2);
@@ -107,8 +106,8 @@ foreach my $directory (@ARGV)
 				print OLDOUTFILE "$oldcount \t";
 				
 				# Print the total number of existing TE predictions for each method, high value first then low
-				my $oldpredictionsmethod1=`grep old "$directory/results/$file1" | wc -l`;
-				my $oldpredictionsmethod2=`grep old $file2 | wc -l`;
+				my $oldpredictionsmethod1=`grep _reference "$directory/results/$file1" | wc -l`;
+				my $oldpredictionsmethod2=`grep _reference $file2 | wc -l`;
 				
 				chomp ($oldpredictionsmethod1);
 				chomp ($oldpredictionsmethod2);
