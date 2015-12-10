984827832d2f51574f14167ec3fecd83c1d402ce
Michael Nelson <42mnelson@gmail.com>
2015-12-10 11:26:20 +0000

    Fix parsing error with certain fq file names

--- scripts/runngstemapper.sh
+++ scripts/runngstemapper.sh
@@ -31,7 +31,11 @@ then
 	then
 		awk -F'[\t;]' -v sample=$sample '{print $1"\t"$2"\t"$3"\t"$6"_"$9"_"sample"_ngs_te_mapper_sr_"NR"\t0\t"$5}' $outputfolder/ngs_te_mapper/bed_tsd/$samplename"insertions.bed" > $outputfolder/ngs_te_mapper/$sample"_ngs_te_mapper_presort.bed"
 	else
-		awk -F'[\t;]' -v sample=$sample '{print $1"\t"$2"\t"$3"\t"$6"_"$9"_"sample"_ngs_te_mapper_sr_"NR"\t0\t"$5}' $outputfolder/ngs_te_mapper/bed_tsd/$sample"_1_"$sample"_2insertions.bed" > $outputfolder/ngs_te_mapper/$sample"_ngs_te_mapper_presort.bed"
+		base1=`basename $fasta1_file`
+		samplename1=${base1%.*}
+		base2=`basename $fasta2_file`
+		samplename2=${base2%.*}
+		awk -F'[\t;]' -v sample=$sample '{print $1"\t"$2"\t"$3"\t"$6"_"$9"_"sample"_ngs_te_mapper_sr_"NR"\t0\t"$5}' $outputfolder/ngs_te_mapper/bed_tsd/$samplename1"_"$samplename2"insertions.bed" > $outputfolder/ngs_te_mapper/$sample"_ngs_te_mapper_presort.bed"
 	fi
 
 	echo -e "track name=\"$sample"_ngs_te_mapper"\" description=\"$sample"_ngs_te_mapper"\"" > $outputfolder/ngs_te_mapper/$sample"_ngs_te_mapper_nonredundant.bed"
