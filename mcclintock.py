d718f38a0bb4472d9011e256013848fa26b1e462
Preston Basting <pjb68507@uga.edu>
2020-04-01 17:27:09 -0400

    trying to fix possible bug related to importing modules in python

--- mcclintock.py
+++ mcclintock.py
@@ -2,11 +2,13 @@
 
 import argparse
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import modules.mccutils as mccutils
 import config.config as config
 import json
 import random
-import sys
+
 
 
 def main():
