a58e7d2de954d6f3de88f763e714d22360ea0dea
Preston Basting <pjb68507@uga.edu>
2020-05-05 12:21:47 -0400

    updating install so perl works in qsub

--- install/scripts/temp.py
+++ install/scripts/temp.py
@@ -2,6 +2,7 @@ import sys
 import os
 sys.path.append(snakemake.config['paths']['mcc_path'])
 import scripts.mccutils as mccutils
+import subprocess
 
 def main():
     print("installing TEMP...")
@@ -32,6 +33,31 @@ def main():
     command = ["patch", "-i", snakemake.params.patch, install_path+"temp/scripts/TEMP_Absence.sh"]
     mccutils.run_command(command,log=snakemake.params.log) 
 
+
+    output = subprocess.Popen(["which", "perl"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    perl_path = output.stdout.read()
+    perl_path = perl_path.decode()
+    perl_path = perl_path.replace("\n","")
+
+    with open(install_path+"temp/scripts/TEMP_Absence.sh", "r") as script:
+        with open(install_path+"tmp", "w") as tmp:
+            for line in script:
+                if "perl" in line and ".pl" in line:
+                    line = line.replace("perl", perl_path)
+                
+                tmp.write(line)
+    
+    mccutils.run_command(["mv",install_path+"tmp", install_path+"temp/scripts/TEMP_Absence.sh"])
+
+    with open(install_path+"temp/scripts/TEMP_Insertion.sh", "r") as script:
+        with open(install_path+"tmp", "w") as tmp:
+            for line in script:
+                if "perl" in line and ".pl" in line:
+                    line = line.replace("perl", perl_path)
+                
+                tmp.write(line)
+
+    mccutils.run_command(["mv",install_path+"tmp", install_path+"temp/scripts/TEMP_Insertion.sh"])
 
     mccutils.remove(install_path+"TEMP-d2500b904e2020d6a1075347b398525ede5feae1")
     mccutils.remove(snakemake.params.zipfile)
