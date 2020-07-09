b0ccd784067fd32cf919e4afd7dac0c141dcd042
pbasting <pjb68507@uga.edu>
2020-07-09 16:50:37 -0400

    updating install to use most recent TEMP commit

--- install/scripts/temp.py
+++ install/scripts/temp.py
@@ -14,25 +14,27 @@ def main():
         print("try running --install with --clean for clean installation")
         sys.exit(1)
 
-    mccutils.remove(snakemake.config['paths']['install']+"TEMP-d2500b904e2020d6a1075347b398525ede5feae1")
+    extracted_file_name = "TEMP-4f67e1da836721a9f0999efa52e1e648fedb75fc"
+
+    mccutils.remove(snakemake.config['paths']['install']+extracted_file_name)
     command = ["unzip", snakemake.params.zipfile]
     mccutils.run_command(command, log=snakemake.params.log)
 
-    mccutils.remove(install_path+"TEMP-d2500b904e2020d6a1075347b398525ede5feae1")
-    command = ["mv", snakemake.config['paths']['install']+"TEMP-d2500b904e2020d6a1075347b398525ede5feae1", install_path]
+    mccutils.remove(install_path+extracted_file_name)
+    command = ["mv", snakemake.config['paths']['install']+extracted_file_name, install_path]
     mccutils.run_command(command, log=snakemake.params.log)
 
     mccutils.remove(install_path+"temp")
     mccutils.mkdir(install_path+"temp")
-    for f in os.listdir(install_path+"TEMP-d2500b904e2020d6a1075347b398525ede5feae1"):
-        command = ["mv", install_path+"TEMP-d2500b904e2020d6a1075347b398525ede5feae1/"+f, install_path+"temp"]
+    for f in os.listdir(install_path+extracted_file_name):
+        command = ["mv", install_path+extracted_file_name+"/"+f, install_path+"temp"]
         mccutils.run_command(command, log=snakemake.params.log)
 
 
     command = ["patch", "-i", snakemake.params.patch, install_path+"temp/scripts/TEMP_Absence.sh"]
     mccutils.run_command(command,log=snakemake.params.log) 
 
-    mccutils.remove(install_path+"TEMP-d2500b904e2020d6a1075347b398525ede5feae1")
+    mccutils.remove(install_path+extracted_file_name)
     mccutils.remove(snakemake.params.zipfile)
 
 if __name__ == "__main__":                
