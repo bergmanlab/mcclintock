37727b64f0c87a7c04021ba2e0bc5c422edc35f4
pbasting <pjb68507@uga.edu>
2020-08-31 17:51:55 -0400

    removing unused js functions, working on optimizing the sorting function

--- templates/js/script.js
+++ templates/js/script.js
@@ -3,58 +3,100 @@ function sortTable(id, n, numeric=false){
     var table = document.getElementById(id);
     var swapped = true;
     var sortDirection = "lowToHigh";
+    var rows,i,j, shouldSwap,x,y,tmp, switchCount = 0;
+    rows = table.rows;
 
-    var rows,i, shouldSwap,x,y, switchCount = 0;
+    console.log("read table");
+    // read in table into an array
+    var rowArray = new Array(rows.length-1);
+    var valuesArray;
+    for (i = 1; i < rows.length; i++){
+        valuesArray = new Array(rows[1].getElementsByTagName("td").length+1);
+        var valuesLength = rows[1].getElementsByTagName("td").length;
+        for (j = 0; j < valuesLength; j++){
+            valuesArray[j] = rows[i].getElementsByTagName("td")[j].innerHTML;
+        }
+        valuesArray[valuesLength] = rows[i].style.display;
+        rowArray[i] = valuesArray;
+    }
+
+    console.log("sort table");
+    // sort array
     while(swapped){
         swapped = false;
-        rows = table.rows;
-        for (i =1; i < (rows.length-1); i++){
-            shouldSwap = false;
-
-            x = rows[i].getElementsByTagName("TD")[n];
-            y = rows[i + 1].getElementsByTagName("TD")[n];
-
+        for (i = 1; i < rowArray.length-1; i++){
+            x = rowArray[i][n]
+            y = rowArray[i+1][n]
             if (sortDirection == "lowToHigh"){
                 if (numeric){
-                    if (parseInt(x.innerHTML) > parseInt(y.innerHTML)){
-                        shouldSwap = true;
+                    if (parseInt(x) > parseInt(y)){
+                        swapped = true;
+                        tmp = rowArray[i];
+                        rowArray[i] = rowArray[i+1];
+                        rowArray[i+1] = tmp;
+                        switchCount += 1;
                         break;
                     }
                 } else {
-                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()){
-                        shouldSwap = true;
+                    if (x.toLowerCase() > y.toLowerCase()){
+                        swapped = true;
+                        tmp = rowArray[i];
+                        rowArray[i] = rowArray[i+1];
+                        rowArray[i+1] = tmp;
+                        switchCount += 1;
                         break;
                     }
                 }
 
             } else if (sortDirection == "highToLow"){
                 if (numeric){
-                    if (parseInt(x.innerHTML) < parseInt(y.innerHTML)){
-                        shouldSwap = true;
+                    if (parseInt(x) < parseInt(y)){
+                        swapped = true;
+                        tmp = rowArray[i];
+                        rowArray[i] = rowArray[i+1];
+                        rowArray[i+1] = tmp;
+                        switchCount += 1;
                         break;
                     }
                 } else {
-                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()){
-                        shouldSwap = true;
+                    if (x.toLowerCase() < y.toLowerCase()){
+                        swapped = true;
+                        tmp = rowArray[i];
+                        rowArray[i] = rowArray[i+1];
+                        rowArray[i+1] = tmp;
+                        switchCount += 1;
                         break;
                     }
                 }
             }
         }
-        if (shouldSwap){
-            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+
+        if (switchCount == 0 && sortDirection == "lowToHigh"){
+            sortDirection = "highToLow";
             swapped = true;
-            switchCount++;
-        } else {
-            console.log(switchCount)
-            console.log(sortDirection)
-            if (switchCount == 0 && sortDirection == "lowToHigh"){
-                sortDirection = "highToLow";
-                swapped = true;
-                console.log(sortDirection);
-            }
         }
     }
+
+    //clear table
+    console.log("clear table");
+    for (i = rows.length-1; i > 0; i--){
+        table.deleteRow(i);
+    }
+
+    console.log("write table");
+    // repolulate table with sorted values
+    for (i = 1; i < rowArray.length; i++){
+        var newRow = table.insertRow(-1);
+        var newCell;
+
+        for (j = 0; j < rowArray[1].length-1; j++){
+            newCell = newRow.insertCell(j);
+            newCell.innerHTML = rowArray[i][j];
+        }
+        newRow.style.display = rowArray[i][rowArray[1].length-1];
+    }
+
+    console.log("fix even-odd column coloring");
     // fix odd even shading
     var rows = table.getElementsByTagName("tr");
     var odd=false;
@@ -73,40 +115,7 @@ function sortTable(id, n, numeric=false){
             }
         }
     }
-}
 
-function filterTable(inputId, tableId, checkbox){
-    var input = document.getElementById(inputId);
-    var filter = input.value.toUpperCase();
-    var table = document.getElementById(tableId);
-    var rows = table.getElementsByTagName("tr");
-    var exactMatch = document.getElementById(checkbox).checked;
-    console.log(exactMatch);
-    var display = false;
-    var data;
-    for (var i = 1; i < rows.length; i++){
-        display = false;
-        for (var j = 0; j < rows[i].getElementsByTagName("td").length; j++){
-            data = rows[i].getElementsByTagName("td")[j];
-            if (data){
-                txtValue = data.textContent || data.innerText;
-                if (exactMatch){
-                    if (txtValue.toUpperCase() == filter){
-                        display = true;
-                    }
-                } else {
-                    if (txtValue.toUpperCase().indexOf(filter) > -1){
-                        display = true;
-                    }
-                }
-            }
-        }
-        if (display){
-            rows[i].style.display = "";
-        } else {
-            rows[i].style.display = "none";
-        }
-    }
 }
 
 function filterTableType(inputId, tableId, exactbox, allbox, refbox, nonrefbox){
