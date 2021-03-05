7bbeb7ef4efcc81dd04aa913eff3e5fc1be84f12
pbasting <pjb68507@uga.edu>
2021-03-05 16:06:00 -0500

    updating teflon and ngs_te_mapper2; adding ngs_te_mapper2 config options

--- install/scripts/teflon.py
+++ install/scripts/teflon.py
@@ -14,19 +14,19 @@ def main():
         print("try running --install with --clean for clean installation")
         sys.exit(1)
     
-    mccutils.remove(snakemake.config['paths']['install']+"TEFLoN-9eca0152f3dd9dc6c44787a30d590f7e321b442c")
+    mccutils.remove(snakemake.config['paths']['install']+"TEFLoN-3e2d67886b70644fd1f7d79263b3c8dbed639e46")
     command = ["unzip", snakemake.params.zipfile]
     mccutils.run_command(command, log=snakemake.params.log)
 
-    mccutils.remove(install_path+"TEFLoN-9eca0152f3dd9dc6c44787a30d590f7e321b442c")
-    command = ["mv", snakemake.config['paths']['install']+"TEFLoN-9eca0152f3dd9dc6c44787a30d590f7e321b442c", install_path]
+    mccutils.remove(install_path+"TEFLoN-3e2d67886b70644fd1f7d79263b3c8dbed639e46")
+    command = ["mv", snakemake.config['paths']['install']+"TEFLoN-3e2d67886b70644fd1f7d79263b3c8dbed639e46", install_path]
     mccutils.run_command(command, log=snakemake.params.log)
 
     mccutils.remove(install_path+"teflon")
     mccutils.mkdir(install_path+"teflon")
 
-    for f in os.listdir(install_path+"TEFLoN-9eca0152f3dd9dc6c44787a30d590f7e321b442c"):
-        command = ["mv", install_path+"TEFLoN-9eca0152f3dd9dc6c44787a30d590f7e321b442c/"+f, install_path+"teflon"]
+    for f in os.listdir(install_path+"TEFLoN-3e2d67886b70644fd1f7d79263b3c8dbed639e46"):
+        command = ["mv", install_path+"TEFLoN-3e2d67886b70644fd1f7d79263b3c8dbed639e46/"+f, install_path+"teflon"]
         mccutils.run_command(command, log=snakemake.params.log)
 
 
@@ -36,7 +36,7 @@ def main():
     command = ["patch","-i", snakemake.params.teflon_patch, install_path+"teflon/teflon.v0.4.py"]
     mccutils.run_command(command, log=snakemake.params.log)
 
-    mccutils.remove(install_path+"TEFLoN-9eca0152f3dd9dc6c44787a30d590f7e321b442c")
+    mccutils.remove(install_path+"TEFLoN-3e2d67886b70644fd1f7d79263b3c8dbed639e46")
     mccutils.remove(snakemake.params.zipfile)
 
     # write version to file
