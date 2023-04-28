34a242e7378a576426d8cece56c61bc29d62bfa2
Casey Bergman <caseybergman@gmail.com>
2023-04-28 16:06:06 -0400

    Update ngs_te_mapper.py
    - updated to use format of later install scripts

--- install/scripts/ngs_te_mapper.py
+++ install/scripts/ngs_te_mapper.py
@@ -7,12 +7,13 @@ def main():
     install_path = snakemake.config['paths']['install']+"/tools/"
 
     raw_name = "ngs_te_mapper-f9f48996ac346ac86d57edbd00534aa1227b753e"
+    method_name = "ngs_te_mapper"
 
     mccutils.remove(snakemake.params.zipfile)
     download_success = mccutils.download(snakemake.params.url, snakemake.params.zipfile, md5=snakemake.params.md5, max_attempts=3)
 
     if not download_success:
-        print("ngs_te_mapper download failed... exiting...")
+        print(method_name+" download failed... exiting...")
         sys.exit(1)
 
     mccutils.remove(snakemake.config['paths']['install']+raw_name)
@@ -23,10 +24,10 @@ def main():
     command = ["mv", snakemake.config['paths']['install']+raw_name, install_path]
     mccutils.run_command(command, log=snakemake.params.log)
 
-    mccutils.remove(install_path+"ngs_te_mapper")
-    mccutils.mkdir(install_path+"ngs_te_mapper")
+    mccutils.remove(install_path+method_name)
+    mccutils.mkdir(install_path+method_name)
     for f in os.listdir(install_path+raw_name):
-        command = ["mv", install_path+raw_name+"/"+f, install_path+"ngs_te_mapper"]
+        command = ["mv", install_path+raw_name+"/"+f, install_path+method_name]
         mccutils.run_command(command, log=snakemake.params.log)  
 
 
@@ -34,7 +35,7 @@ def main():
     mccutils.remove(snakemake.params.zipfile)
 
     # write version to file
-    with open(install_path+"ngs_te_mapper/version.log","w") as version:
+    with open(install_path+"/"+method_name+"/version.log","w") as version:
         version.write(snakemake.params.md5)
 
 if __name__ == "__main__":                
