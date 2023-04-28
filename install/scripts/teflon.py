cfe73c820f48c21350bf39cef9722619d77ac46f
Casey Bergman <caseybergman@gmail.com>
2023-04-28 16:36:08 -0400

    Update teflon.py

--- install/scripts/teflon.py
+++ install/scripts/teflon.py
@@ -6,35 +6,38 @@ import scripts.mccutils as mccutils
 def main():
     install_path = snakemake.config['paths']['install']+"/tools/"
 
+    raw_name="TEFLoN-3e2d67886b70644fd1f7d79263b3c8dbed639e46"
+    method_name = "teflon"
+
     mccutils.remove(snakemake.params.zipfile)
     download_success = mccutils.download(snakemake.params.url, snakemake.params.zipfile, md5=snakemake.params.md5, max_attempts=3)
 
     if not download_success:
-        print("teflon download failed... exiting...")
+        print(method_name+" download failed... exiting...")
         sys.exit(1)
     
-    mccutils.remove(snakemake.config['paths']['install']+"TEFLoN-3e2d67886b70644fd1f7d79263b3c8dbed639e46")
+    mccutils.remove(snakemake.config['paths']['install']+raw_name)
     command = ["unzip", snakemake.params.zipfile]
     mccutils.run_command(command, log=snakemake.params.log)
 
-    mccutils.remove(install_path+"TEFLoN-3e2d67886b70644fd1f7d79263b3c8dbed639e46")
-    command = ["mv", snakemake.config['paths']['install']+"TEFLoN-3e2d67886b70644fd1f7d79263b3c8dbed639e46", install_path]
+    mccutils.remove(install_path+raw_name)
+    command = ["mv", snakemake.config['paths']['install']+raw_name, install_path]
     mccutils.run_command(command, log=snakemake.params.log)
 
     mccutils.remove(install_path+"teflon")
     mccutils.mkdir(install_path+"teflon")
 
-    for f in os.listdir(install_path+"TEFLoN-3e2d67886b70644fd1f7d79263b3c8dbed639e46"):
-        command = ["mv", install_path+"TEFLoN-3e2d67886b70644fd1f7d79263b3c8dbed639e46/"+f, install_path+"teflon"]
+    for f in os.listdir(install_path+raw_name):
+        command = ["mv", install_path+raw_name+"/"+f, install_path+"teflon"]
         mccutils.run_command(command, log=snakemake.params.log)
 
-    command = ["patch","-i", snakemake.params.pseudo2refConvert_patch, install_path+"teflon/teflon_scripts/pseudo2refConvert.py"]
+    command = ["patch","-i", snakemake.params.pseudo2refConvert_patch, install_path+method_name+"/teflon_scripts/pseudo2refConvert.py"]
     mccutils.run_command(command, log=snakemake.params.log)
 
-    command = ["patch","-i", snakemake.params.teflon_patch, install_path+"teflon/teflon.v0.4.py"]
+    command = ["patch","-i", snakemake.params.teflon_patch, install_path+method_name+"/teflon.v0.4.py"]
     mccutils.run_command(command, log=snakemake.params.log)
 
-    mccutils.remove(install_path+"TEFLoN-3e2d67886b70644fd1f7d79263b3c8dbed639e46")
+    mccutils.remove(install_path+raw_name)
     mccutils.remove(snakemake.params.zipfile)
 
     # write version to file
