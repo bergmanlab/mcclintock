4a38bdf0857ca4cf3b05e909cece2796e5ab95d8
Casey Bergman <caseybergman@gmail.com>
2023-05-01 09:02:04 -0400

    Update te-locate.py
    - update variable naming for consistency with other install scripts
    - add comments to install script

--- install/scripts/te-locate.py
+++ install/scripts/te-locate.py
@@ -4,18 +4,26 @@ sys.path.append(snakemake.config['paths']['mcc_path'])
 import scripts.mccutils as mccutils
 
 def main():
+    #set up installation path and variable names for component method
+    install_path = snakemake.config['paths']['install']+"tools/"
+
+    method_name = "te-locate"
+
+    #download component method source code and check for integrity
+    #note: snakemake.params are found in /install/Snakefile
     mccutils.remove(snakemake.params.tar)
     download_success = mccutils.download(snakemake.params.url, snakemake.params.tar, md5=snakemake.params.md5, max_attempts=3)
 
     if not download_success:
-        print("TE-Locate download failed... exiting...")
+        print(method_name+" download failed... exiting...")
         sys.exit(1)
 
-    command = ["tar", "-xvf", snakemake.params.tar, "-C", snakemake.config['paths']['install']+"/tools/te-locate/"]
+    #unpack component method source code into component method directory
+    command = ["tar", "-xvf", snakemake.params.tar, "-C", install_path+method_name]
     mccutils.run_command(command, log=snakemake.params.log)
 
-    # write version to file
-    with open(snakemake.config['paths']['install']+"/tools/te-locate/version.log","w") as version:
+    #write version to file
+    with open(install_path+method_name+"/version.log","w") as version:
         version.write(snakemake.params.md5)
 
 if __name__ == "__main__":                
